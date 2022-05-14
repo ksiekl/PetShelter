950c9e422cfb4abaad2905e77858d1c7450f1df8
Katarina Sieklova <xsieklov@fi.muni.cz>
2022-05-14 19:41:42 +0200

    Admin side #2

--- PetShelter/Data/PetService.cs
+++ PetShelter/Data/PetService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PetShelter.Pages;
 
 namespace PetShelter.Data;
 
@@ -14,7 +15,32 @@ public class PetService
     {
         return await _context.Pets.ToListAsync();
     }
-    
+
+    public async Task<List<Pet>> GetAvailablePets()
+    {
+        var deliveredPets = _context.Pets
+            .Join(_context.Deliveries, pet => pet.Id, delivery => delivery.NewPetId,
+                (pet, delivery) => new Tuple<Pet, Delivery>(pet, delivery))
+            .AsEnumerable()
+            .Where(p => p.Item2.Status == Status.Finished)
+            .Select(pet => pet.Item1)
+ 
+            .ToHashSet();
+
+        var adoptedPets =  _context.Pets
+            .Join(_context.Adoptions, pet => pet.Id, adoption => adoption.PetId,
+                (pet, adoption) => new Tuple<Pet, PetAdoption>(pet, adoption))
+            .AsEnumerable()
+            .Where(p => p.Item2.Status != Status.Declined)
+            .Select(pet => pet.Item1)
+            
+            .ToHashSet();
+
+        deliveredPets.ExceptWith(adoptedPets);
+        return deliveredPets.ToList();
+
+    }
+
     public async Task AddPet(Pet pet)
     {
         _context.Pets.Add(pet);
@@ -24,5 +50,13 @@ public class PetService
     public Pet GetOnePet(int petId)
     {
         return Enumerable.FirstOrDefault(_context.Pets, pet => pet.Id == petId)!;
+    }
+
+    public async Task<List<Pet>> GetUsersAdoptions(string userId)
+    {
+       return await _context.Adoptions
+            .Where(adoption => adoption.Status == Status.Finished && adoption.UserId == userId)
+            .Join(_context.Pets, a => a.PetId, p => p.Id, (adoption, pet) => pet)
+            .ToListAsync();
     }
 }
